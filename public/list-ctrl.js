9861b82dbc0bb987794025192b228411f78df355
aws1718maab <mairelis.arroyo@gmail.com>
2018-04-26 17:13:21 +0000

    Addes support to MongoDB instead

--- public/list-ctrl.js
+++ public/list-ctrl.js
@@ -9,6 +9,12 @@ angular
                 $scope.contacts = response.data;
             });
         }
+         
+        function.getCon = function(){ //Funcion para obtener contactos, buscar.
+            $http.get("/api/v1/contacts/" + $scope.newContact.name).then(function (response){
+                $scope.contacts = response.data;
+            });
+        }
         
         $scope.addContact = function (){
             
@@ -17,9 +23,34 @@ angular
                 .then(function (){
                     refresh();  
                 });
-        
+        }
+        $scope.delallContact = function (){
+            
+            $http
+                .delete("/api/v1/contacts", $scope.delallContact)
+                .then(function (){
+                    refresh();  
+                });
+            
         }
         
-        refresh();
+        $scope.delContact = function (){
+            
+            $http
+                .delete("/api/v1/contacts/" + $scope.newContact.name, $scope.delContact)
+                .then(function (){
+                    refresh();  
+                });
+        }
         
+        $scope.updateContacto = function(){
+            $http
+                .put("/api/v1/contacts/"+$scope.newContact.name, {name:$scope.newContact.name, phone:$scope.newContact.phone})
+                .then (function() {
+                    refresh();
+                });
+            
+        }
+        refresh();
     });
+    
